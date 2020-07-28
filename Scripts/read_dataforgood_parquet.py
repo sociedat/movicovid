57d5d6061edca809150ebb27b07c0482a8f3e728
elozadaj <elozadaj@gmail.com>
2020-07-28 06:58:53 +0000

    Modifying EDA script

--- Scripts/read_dataforgood_parquet.py
+++ Scripts/read_dataforgood_parquet.py
@@ -2,23 +2,42 @@ import boto3
 import io
 import pandas as pd
 import pyarrow.parquet as pq
-import s3fs
+import s3fs client = boto3.client('s3')
 
 
-client = boto3.client('s3')
 s3 = s3fs.S3FileSystem()
 
-# Returns the parquet file corresponding to the given key as a pandas dataframe.
-def get_parquet_file(key):
-    print(key)
-    return pq.ParquetDataset('s3://movicovid/' + key, filesystem=s3).read_pandas().to_pandas()
+# Extracts the date from a given key.
+def get_parquet_date(key):
+    index = key.index('fecha') + 6
+    return key[index:index+10]
 
-# Get all parquet objects stored in raw/dataforgood. 
-parquet_objects = client.list_objects_v2(Bucket='movicovid', Prefix='transformed/dataforgood')
-parquet_keys = [item['Key'] for item in parquet_objects['Contents'] if item['Key'].endswith('.parquet')]
-pandas_dataframes = []
-for key in parquet_keys:
-    pandas_dataframes.append(get_parquet_file(key))
+# Returns a pandas dataframe corresponding to the parquet file of the given key.
+def get_parquet_file(key):
+    print('Getting parquet file located at: ' + key)
+    dataframe = pq.ParquetDataset('s3://movicovid/' + key, filesystem=s3) \
+            .read_pandas() \
+            .to_pandas()
+    # Doing this as ParquetDataset.read(columns = [expected_columns]) does not
+    # recognize 'columns' parameter :( and we need 'fecha' (partition column).
+    parquet_file_date = get_parquet_date(key)
+    date_df = [parquet_file_date for i in range(len(dataframe.index))]
+    dataframe['fecha'] = date_df
+
+    return dataframe
+
+# Reads all the dataforgood data contained in the corresponding S3 bucket.
+def get_dataforgood_data():
+    # Get all parquet objects stored in raw/dataforgood.
+    parquet_objects = client.list_objects_v2(
+        Bucket='movicovid', Prefix='transformed/dataforgood')
+    parquet_keys = [item['Key'] for item in parquet_objects['Contents'] \
+                    if item['Key'].endswith('.parquet')]
+    pandas_dataframes = []
+    for key in parquet_keys:
+        pandas_dataframes.append(get_parquet_file(key))
+
+    return pd.concat(pandas_dataframes)
 
 # This is the variable containing all the data from dataforgood.
-movicovid_data = pd.concat(pandas_dataframes)
+dataforgood_data = get_dataforgood_data()
